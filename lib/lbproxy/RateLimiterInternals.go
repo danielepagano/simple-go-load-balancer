abc38cbf0bdaaf97f0a43ba547a0165f7e3bb38f
Daniele Pagano <daniele@danielepagano.com>
2022-09-04 14:24:36 -0600

    Optimized and fixed rate limiter

--- lib/lbproxy/RateLimiterInternals.go
+++ lib/lbproxy/RateLimiterInternals.go
@@ -2,6 +2,7 @@ package lbproxy
 
 import (
 	"log"
+	"sort"
 	"sync"
 	"time"
 )
@@ -19,13 +20,17 @@ type rlManager struct {
 }
 
 func CreateRateLimitManager(tag string, config RateLimitManagerConfig) *rlManager {
-	return &rlManager{
+	rlm := &rlManager{
 		tag:                    tag,
 		config:                 config,
 		currentOpenConnections: 0,
 		addedTimestamps:        []int64{},
-		currentTime:            time.Now().Unix, // Current time is normally wall time, but can be changed for testing
 	}
+	// Current time is normally wall time, but can be changed for testing
+	rlm.currentTime = func() int64 {
+		return time.Now().Unix()
+	}
+	return rlm
 }
 
 // overrideTimeSupplier is an internal method to supply a function to mock the passage of time for testing
@@ -49,6 +54,7 @@ func (m *rlManager) AddConnection() bool {
 	if m.config.MaxRateAmount >= 0 && len(m.addedTimestamps) >= m.config.MaxRateAmount {
 		// +1 because if e.g. if we allow 1 event/sec, window will start at current time, because this timestamp has been already used
 		windowStart := currentTs - m.config.MaxRatePeriodSeconds + 1
+
 		// trim items outside of window
 		m.addedTimestamps = trimTimestamps(m.addedTimestamps, windowStart)
 		if len(m.addedTimestamps) >= m.config.MaxRateAmount {
@@ -78,20 +84,18 @@ func (m *rlManager) ReleaseConnection() {
 	log.Println("RLM-", m.tag, "open:", m.currentOpenConnections, "ts:", m.addedTimestamps)
 }
 
+// trimTimestamps removed timestamps from ts that are older than windowStart
+// elements in ts are added serially by current time, so it is an array guaranteed to be sorted
 func trimTimestamps(ts []int64, windowStart int64) []int64 {
-	newStart := 0
-	for i, t := range ts {
-		newStart = i
-		if t >= windowStart {
-			break
-		}
-	}
-
-	// Window was fully purged
-	if newStart >= len(ts) || ts[newStart] < windowStart {
+	// If last element is already too old, entire list can be cleared
+	tsLen := len(ts)
+	if tsLen == 0 || ts[tsLen-1] < windowStart {
 		return []int64{}
 	}
 
-	// Window was partially purged
+	// Use a standard binary search to find index of first item in window
+	newStart := sort.Search(tsLen, func(i int) bool { return ts[i] >= windowStart })
+
+	// Purge elements before windowStart
 	return ts[newStart:]
 }
