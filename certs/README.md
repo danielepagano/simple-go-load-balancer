20d500258985920eb695e86511b637411344e072
Daniele Pagano <daniele@danielepagano.com>
2022-09-05 21:29:07 -0600

    Added test, but problem

--- certs/README.md
+++ certs/README.md
@@ -3,7 +3,7 @@
 ## Generation
 In this sample project, we have generated sample certificates locally as follows
 
-1. A CA certificate locally using`openssl req -newkey rsa:2048 -nodes -x509 -days 30 -out ca.crt -keyout ca.key` (plus subject details)
+1. A CA certificate locally using `openssl req -newkey rsa:2048 -nodes -x509 -days 30 -out ca.crt -keyout ca.key` (plus subject details)
 2. A Server key `openssl genrsa -out server.key 2048`
 3. A Certificate Signing Request `openssl req -new -key server.key -days 30 -out server.csr` (plus subject details)
 4. Sign CSR with `openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 30 -sha384`
@@ -17,10 +17,9 @@ In this sample project, we have generated sample certificates locally as follows
 
 These are located in the `certs/` directory
 
-- CA certs: `ca.crt` and `ca.key` from step #1, issued to CA `danielepagano.com`
-- `server.key` from step #2
-- `server.csr` from step #3
-  - CN=`danielepagano.com`, challenge password `5TA&K#&nWE4oIcQf`
+- CA certs: `ca.crt` and `ca.key` from step #1, issued to CA `localhost`
+- `server.key` from step #2 using `openssl req -new -key server.key -days 30 -out server.csr -subj "/C=US/ST=UT/L=SLC/O=Tets/CN=*.localhost"`
+- `server.csr` from step #3 using `openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 30 -sha384 -extfile <(printf "subjectAltName=DNS:localhost")`
 - `server.crt` from step $4, valid for Sept 2022
 
 ### Client certs for "localhost"
@@ -29,16 +28,16 @@ These are under the `certs/clients` directory, plus a directory for the client C
 We use a simple convention in the server where the client id is the file name,
 for example for `CN=localhost` you would use `certs/clients/localhost/localhost.crt` etc.
 
-1. `openssl genrsa -out localhost.key 2048`
-2. `openssl req -new -key localhost.key -days 30 -out localhost.csr` 
-   - CN=`localhost`, challenge password `EQmvv^BY$L1n9Pa#`
-3. `openssl x509 -req -in localhost.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out localhost.crt -days 30 -sha384`
+From `certs` folder
+
+1. `openssl genrsa -out clients/localhost/localhost.key 2048`
+2. `openssl req -new -key clients/localhost/localhost.key -days 30 -out clients/localhost/localhost.csr -subj "/C=US/ST=UT/L=SLC/O=Tets/CN=localhost"`
+3. `openssl x509 -req -in clients/localhost/localhost.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out clients/localhost/localhost.crt -days 30 -sha384 -extfile <(printf "subjectAltName=DNS:localhost")`
 
 Simple test from command line
 ```shell
-curl --trace trace.log -k \
-	--cacert ./certs/ca.crt \
-	--cert ./certs/clients/localhost/localhost.crt \
-	--key ./certs/clients/localhost/localhost.key \
+curl -k --cacert ca.crt \
+	--cert clients/localhost/localhost.crt \
+	--key clients/localhost/localhost.key \
 	https://localhost:9001
 ```
---
+++ internal/security/Authentication_test.go
@@ -0,0 +1,122 @@
+package security
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+	"sync"
+	"testing"
+)
+
+// Warning: these certs will expire after Sept 2022
+// In non-sample app, we would be generating these certs directly in the test
+var config = ServerSecurityConfig{
+	EnableMutualTLS: true,
+	CertPath:        "../../certs",
+	ClientsCertPath: "../../certs/clients",
+	CertFileExt:     ".crt",
+	CertKeyExt:      ".key",
+	CaCertName:      "ca",
+	ServerCertName:  "server",
+}
+
+const clientId = "localhost"
+const serverAddress = "localhost:9001"
+
+func TestMTLSAuthenticationProvider(t *testing.T) {
+	auth, tlsListener := startTLSListener(t)
+	defer tlsListener.Close()
+	if tlsListener == nil {
+		return
+	}
+
+	clientConfig := loadClientTLSConfig(t, clientId)
+	if clientConfig == nil {
+		return
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		clientConn, acceptErr := tlsListener.Accept()
+		defer clientConn.Close()
+		defer wg.Done()
+
+		log.Println("TLS listener accepted incoming")
+		if acceptErr != nil {
+			t.Errorf("Could not accept incoming error = %v", acceptErr)
+		}
+		authClientId, acceptErr := auth.AuthenticateConnection(clientConn)
+		if acceptErr != nil {
+			t.Errorf("Could not authenticate incoming error = %v", acceptErr)
+			return
+		}
+		if authClientId != clientId {
+			t.Errorf("Inccorecct clientId: %v", authClientId)
+			return
+		}
+		log.Println("SUCCESS: authenticated clientId", authClientId)
+	}()
+
+	conn, err := tls.Dial("tcp", serverAddress, clientConfig)
+	defer conn.Close()
+	if err != nil {
+		t.Errorf("could not connect to server err = %v", err)
+		return
+	}
+
+	_, err = io.WriteString(conn, "Hello mTLS\n")
+	if err != nil {
+		t.Errorf("could not write to server = %v", err)
+		return
+	}
+
+	wg.Wait()
+}
+
+func loadClientTLSConfig(t *testing.T, clientId string) *tls.Config {
+	// Load CA certificate.
+	caCrt := filepath.Join(config.CertPath, config.CaCertName+config.CertFileExt)
+	caCert, err := os.ReadFile(caCrt)
+	if err != nil {
+		t.Errorf("could not load CA cert = %v", err)
+		return nil
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Load Client certificate
+	clientCrt := filepath.Join(config.ClientsCertPath, clientId, clientId+config.CertFileExt)
+	clientKey := filepath.Join(config.ClientsCertPath, clientId, clientId+config.CertKeyExt)
+	clientCert, err := tls.LoadX509KeyPair(clientCrt, clientKey)
+	if err != nil {
+		t.Errorf("could not load client cert = %v", err)
+		return nil
+	}
+
+	return &tls.Config{
+		RootCAs:      caCertPool,
+		Certificates: []tls.Certificate{clientCert},
+	}
+}
+
+func startTLSListener(t *testing.T) (AuthenticationProvider, net.Listener) {
+	auth, err := NewMTLSAuthenticationProvider(config)
+	if err != nil {
+		t.Errorf("TestMTLSAuthenticationProvider() error = %v", err)
+		return nil, nil
+	}
+
+	tlsListener, err := auth.StartListener(serverAddress)
+	if err != nil {
+		t.Errorf("TestMTLSAuthenticationProvider() error = %v", err)
+		return nil, nil
+	}
+	log.Println("TLS Listener started on", tlsListener.Addr().String())
+	return auth, tlsListener
+}
